6d8641259617f9bbbc8e176716b6412c16d8f38a
Ryo Fusamae <ryofusamae@gmail.com>
2016-08-29 19:58:11 -0700

    more changes

--- lesson_2/mortgage_calc.rb
+++ lesson_2/mortgage_calc.rb
@@ -1,49 +1,47 @@
 puts("Welcome to mortgage calculator!")
 
 loop do
-loan_amn = ''
+  loan_amn = ''
 
-loop do
-  puts("Please enter the amount of loan")
-  loan_amn = gets.chomp.to_f
-  if loan_amn < 0
-    puts("Please enter a positive number")
-  else
-    break
+  loop do
+    puts("Please enter the amount of loan")
+    loan_amn = gets.chomp.to_f
+    if loan_amn < 0
+      puts("Please enter a positive number")
+    else
+      break
+    end
   end
-end
 
-apr_in_float = ''
+  apr_in_float = ''
 
-loop do
-  puts("Please enter the Annual Percentage Rate")
-  apr = gets.chomp.to_i
+  loop do
+    puts("Please enter the Annual Percentage Rate")
+    apr = gets.chomp.to_i
 
-  if apr > 100
-    puts("That's impossible!!")
-  else
-    apr_in_float = apr.to_f / 100
-    break
+    if apr > 100
+      puts("That's impossible!!")
+    else
+      apr_in_float = apr.to_f / 100
+      break
+    end
   end
-end
 
+  puts("Please enter the loan duration in months")
 
-puts("Please enter the loan duration in months")
+  loan_dur = gets.chomp.to_i
 
-loan_dur = gets.chomp.to_i
+  monthly_int_rate = apr_in_float / 12
 
-monthly_int_rate = apr_in_float / 12
+  monthly_payment =
+    loan_amn * (monthly_int_rate / (1 - (1 + monthly_int_rate)**-loan_dur))
 
-monthly_payment = loan_amn * (monthly_int_rate / (1 - (1 + monthly_int_rate)**-loan_dur))
+  puts("Your monthly payment is #{monthly_payment}")
+  puts("Would you like to calculate again?")
 
-puts("Your monthly payment is #{monthly_payment}")
+  answer = gets.chomp
 
-puts("Would you like to calculate again?")
-
-answer = gets.chomp
-
-break unless answer.downcase.start_with?("y")
-
+  break unless answer.downcase.start_with?("y")
 end
 
 puts("Thank you for using the calculator!")
--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,17 +1,26 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = %w(rock paper scissors lizzard spock)
 
 def prompt(message)
   Kernel.puts(message)
 end
 
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'Paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper') ||
+    (first == 'lizzard' && second == 'spock') ||
+    (first == 'spock' && second == 'scissors') ||
+    (first == 'rock' && second == 'lizzard') ||
+    (first == 'paper' && second == 'spock') ||
+    (first == 'scissors' && second == 'lizzard') ||
+    (first == 'lizzard' && second == 'paper') ||
+    (first == 'spock' && second == 'rock')
+end
+
 def display_results(player, computer)
-  if (player == 'rock' && computer == 'scissors') ||
-      (player == 'Paper' && computer == 'rock') ||
-        (player == 'scissors' && computer == 'paper')
+  if win?(player, computer)
     prompt("You win!")
-  elsif (player == 'rock' && computer == 'paper') ||
-          (player == 'paper' && computer == 'scissors') ||
-            (player == 'scissors' && computer == 'rock')
+  elsif win?(computer, player)
     prompt("Computer win!")
   else
     prompt("It's a tie!")
@@ -21,7 +30,6 @@ end
 prompt("Welcome!")
 
 loop do
-
   choice = ''
 
   loop do
