2722d96a625b59c8ca83bc86305f116155c42309
Administrator <administrator@MacBook.local>
2016-09-18 13:00:18 -0700

    tic tac toe

--- lesson_4/tictactoe.rb
+++ lesson_4/tictactoe.rb
@@ -1,18 +1,25 @@
-#tictactoe.rb
+require 'pry'
 
-#1. display the initial empty board
-#2. ask user for input
-#3. ask computer for input
-#4. display updated board
-#5. if winner, display winner
-#6. if the board is full, display board
-#7. if neither winner or the board is full, repeat 2 - 6.
-#8. play again?
-#9. Goodbye!
+INITIAL_MARKER = " "
+PLAYER_MARKER = "X"
+COMPUTER_MARKER = "0"
 
+WINNING_PATTERN = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
+                  [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
+                  [[1, 5, 9], [3, 5, 7]]
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
 def display_board(brd)
-  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  system 'clear'
+  puts "You're a #{PLAYER_MARKER}"
+  puts "Computer is a #{COMPUTER_MARKER}"
+  puts " "
   puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
   puts "     |     |"
   puts "-----+-----+-----"
   puts "     |     |"
@@ -22,13 +29,75 @@ def display_board(brd)
   puts "     |     |"
   puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
   puts "     |     |"
+  puts " "
 end
+# rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
 def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = ' ' }
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
-board = initialize_board
-display_board(board)
+def empty_square(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def user_places_piece!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square (#{empty_square(brd).join(', ')})"
+    square = gets.chomp.to_i
+    break if empty_square(brd).include?(square)
+    prompt "Sorry, that's not a valid choice"
+  end
+  brd[square] = PLAYER_MARKER
+end
+
+def computer_places_piece!(brd)
+  square = empty_square(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_square(brd).empty?
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd)
+end
+
+def detect_winner(brd)
+  WINNING_PATTERN.each do |line|
+    if brd.value_at(line[0], line[1], line[2]).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.value_at(line[0], line[1], line[2]).count(COMPUTER_MARKER) == 3
+      return 'Computer'
+    end
+  end
+  nil
+end
+
+loop do
+  board = initialize_board
+  display_board(board)
+
+  loop do
+    user_places_piece!(board)
+    computer_places_piece!(board)
+    display_board(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie."
+  end
+
+  prompt "Would you like to play it again? (y or n)"
+  play_again = gets.chomp
+  break unless play_again.downcase.start_with?("y")
+end
+
+prompt "Thanks for playing!"
