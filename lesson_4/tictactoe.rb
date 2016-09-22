d64c4ab27f47ddd77738188f4b289677b8815cc7
Administrator <administrator@MacBook.local>
2016-09-22 06:27:38 -0700

    TTT Bonus features

--- lesson_4/tictactoe.rb
+++ lesson_4/tictactoe.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
-INITIAL_MARKER = " "
-PLAYER_MARKER = "X"
-COMPUTER_MARKER = "0"
-
+INITIAL_MARKER = " ".freeze
+PLAYER_MARKER = "X".freeze
+COMPUTER_MARKER = "0".freeze
 WINNING_PATTERN = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
                   [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
                   [[1, 5, 9], [3, 5, 7]]
+SETTING = "choice".freeze
 
 def prompt(message)
   puts "=> #{message}"
@@ -56,7 +56,23 @@ def user_places_piece!(brd)
 end
 
 def computer_places_piece!(brd)
-  square = empty_square(brd).sample
+  square = nil
+  WINNING_PATTERN.each do |line|
+    square = find_at_risk_square(line, brd, PLAYER_MARKER)
+    break if square
+  end
+
+  if !square
+    WINNING_PATTERN.each do |line|
+      square = find_at_risk_square(line, brd, COMPUTER_MARKER)
+      break if square
+    end
+  end
+
+  if !square
+    square = empty_square(brd).sample
+  end
+
   brd[square] = COMPUTER_MARKER
 end
 
@@ -79,26 +95,63 @@ def detect_winner(brd)
   nil
 end
 
-loop do
+def who_starts_first?(choice)
+  setting = nil
+  loop do
+    if choice == 'choice'
+      prompt "Enter 'computer' for the computer to start or 'player' for player to start"
+      setting = gets.chomp.to_sym
+      break if setting == :computer || setting == :player
+      prompt "Invalid Choice."
+    end
+  end
+  setting
+end
 
+def place_piece!(brd, curr_player)
+  if curr_player == :computer
+    computer_places_piece!(brd)
+  elsif curr_player == :player
+    user_places_piece!(brd)
+  end
+end
+
+def alternate_player(curr_player)
+  if curr_player == :computer
+    return :player
+  else
+    return :computer
+  end
+end
+
+def find_at_risk_square(line, board, marker)
+  if board.values_at(*line).count(marker) == 2
+    board.select { |k, v| line.include?(k) && v == INITIAL_MARKER }.keys.first
+  else
+    nil
+  end
+end
+
+# game starts here
+
+loop do
   player_score = 0
   computer_score = 0
 
   loop do
-
     board = initialize_board
+    current_player = who_starts_first?(SETTING)
     display_board(board)
 
     loop do
-      user_places_piece!(board)
       display_board(board)
-      break if someone_won?(board) || board_full?(board)
-      computer_places_piece!(board)
-      display_board(board)
+      place_piece!(board, current_player)
+      current_player = alternate_player(current_player)
       break if someone_won?(board) || board_full?(board)
     end
 
     if someone_won?(board)
+      display_board(board)
       prompt "#{detect_winner(board)} +1!"
       if detect_winner(board) == "Player"
         player_score += 1
@@ -114,20 +167,31 @@ loop do
 
     break if player_score == 5 || computer_score == 5
 
-    prompt "keep playing? (y/n)"
-    answer = gets.chomp
-    break unless answer.downcase.start_with?("y")
+    answer = ""
+
+    loop do
+      prompt "keep playing? (y/n)"
+      answer = gets.chomp
+      if answer.downcase.start_with?("n")
+        break
+      elsif answer.downcase.start_with?("y")
+        break
+      else
+        prompt "Please enter a valid input"
+      end
+    end
+    break if answer.downcase.start_with?("n")
   end
 
   if player_score == 5
     prompt "You Win!"
-  else
+  elsif computer_score == 5
     prompt "Computer wins!"
   end
 
-  prompt "Play again? (y/n)"
+  prompt "Start from the beginning? (y/n)"
   play_again = gets.chomp
-  break unless play_again.downcase.start_with("y")
+  break unless play_again.downcase.start_with?("y")
 end
 
 prompt "Thanks for playing!"
